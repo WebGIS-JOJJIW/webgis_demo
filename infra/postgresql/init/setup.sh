6e410f4b63e9685ae386c2c6547eda36a88971d7
Kittisak Sajjapngse <kittisak.sajjapongse@gmail.com>
2024-06-22 11:39:35 -0400

    Added postgres init script

---
+++ infra/postgresql/init/setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
+SQL_SCRIPT_DIR=$SCRIPT_DIR/scripts
+
+PSQL="psql"
+USER="-U admin"
+POSTGRES_DATABASE="-d postgres"
+GEOSERVER_DATABASE="-d gis_db"
+IMAGE_DATABASE="-d image_db"
+
+SQL_CREATE_IMAGE_TABLE="
+CREATE TABLE still_images (
+    id SERIAL PRIMARY KEY,
+    sensor_id VARCHAR(64) NOT NULL,
+    image_url VARCHAR(256) NOT NULL,
+    captured_ts TIMESTAMP NOT NULL,
+    created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+"
+
+set -x
+# set -e
+docker exec -it gis-db $PSQL $USER $GEOSERVER_DATABASE -c "CREATE DATABASE image_db"
+docker exec -it gis-db $PSQL $USER $POSTGRES_DATABASE -c "CREATE DATABASE image_db"
+docker exec -it gis-db $PSQL $USER $IMAGE_DATABASE -c "$SQL_CREATE_IMAGE_TABLE"
