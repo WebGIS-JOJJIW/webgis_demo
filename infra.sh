19462f7046817ad9704ba7bbeb90922b50cb32e2
kittisak-sajjapongse <63345597+kittisak-sajjapongse@users.noreply.github.com>
2024-06-22 21:42:12 -0400

    Creating ingester (0th round) (#4)

---
+++ infra.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/bash
+set -e
+
+function up {
+    set -x
+    docker compose -f infra/docker-compose.yml build
+    docker compose -f infra/docker-compose.yml up -d
+    set +x
+}
+
+function setup {
+    up
+    set -x
+    # Wait for the services to finish initialization
+    echo Wait 90 seconds to wait for services to finish initialization
+    sleep 90
+
+    # Setup PostgreSQL
+    infra/postgresql/init/setup.sh
+
+    # Setup GeoServer
+    infra/geoserver/init/setup.sh
+    set +x
+}
+
+function reset {
+    down
+    set -x
+    sudo git clean -Xfd
+    sudo docker rmi artifact_serve:latest
+    set +x
+}
+
+function down {
+    set -x
+    docker compose -f infra/docker-compose.yml down
+    set +x
+}
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 {up|down|setup|reset}"
+    exit 1
+fi
+
+case $1 in
+    up)
+        up
+        ;;
+    down)
+        down
+        ;;
+    setup)
+        setup
+        ;;
+    reset)
+        reset
+        ;;
+    *)
+        echo "Invalid argument. Usage: $0 {up|down|setup|reset}"
+        exit 1
+        ;;
+esac
