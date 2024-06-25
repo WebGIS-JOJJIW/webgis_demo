e773c23b2c9416bf5bfb4bae32747f5ccb8f7779
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2024-06-24 21:38:02 -0400

    Fixed py3 script to install deps

--- py3.sh
+++ py3.sh
@@ -7,4 +7,4 @@ docker run -it \
     --network webgis_net \
     -v $SCRIPT_DIR/:/data \
     --workdir /data \
-    python:3.12.4-alpine3.20 sh
+    python:3.12.4-alpine3.20 sh -c "pip3 install -r /data/requirements.txt; sh"
