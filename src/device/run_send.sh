dcac7b3f3b3337ee8f18189d96742c4e30c1630f
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2024-07-09 05:44:36 +0700

    Allowed user to specify broker for device send example

--- src/device/run_send.sh
+++ src/device/run_send.sh
@@ -4,8 +4,13 @@ SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 
 if [ "$#" -eq 0 ]; then
     echo "Error: No arguments provided."
-    echo "Usage: $0 <path_to_image>"
+    echo "Usage: $0 <path_to_image> [optional_broker_address]"
     exit 1
 fi
 
-${SCRIPT_DIR}/../../py3.sh send_pic.py -i /data/src/device/$@
+BROKER_ARG=""
+if [ ! -z "$2" ]; then
+    BROKER_ARG="-b $2"
+fi
+
+${SCRIPT_DIR}/../../py3.sh send_pic.py -i /data/src/device/$1 ${BROKER_ARG}
