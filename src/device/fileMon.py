b8411f67b607e8d6a684622e821eab0e6b9bb419
jeerasak.rua <jeerasak.rua@gistda.or.th>
2024-07-13 14:57:44 +0000

    using util and configuration file

--- src/device/fileMon.py
+++ src/device/fileMon.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python
 # ==========================================================================================
 # Script Name: fileMon.py
+# Script Desc: POC - Monitoring incoming file extension from the specified directory then send it
+#   over message broker to the webgis server.
+#
 # Dev by : Sutee C.
-# Release Version : 0.1
+# Version : 1.0
 # ==========================================================================================
 import os
 import time
@@ -13,13 +16,23 @@ from inotify_simple import INotify, flags
 from datetime import datetime
 from pathlib import Path
 from channel import Channel
+
 from pb.response_pb2 import Response, SourceType
+from util_fileMon import CodeBlockTimer, Config
 
 def get_datetime_from_timestamp(timestamp: int) -> str:
     return datetime.fromtimestamp(timestamp).strftime("%Y/%m/%d %H-%M-%S")
 
 def send_file_over_mbroker(image: str, sensor: str):
-    hostip = "163.47.10.22"
+    if 'mqhost' not in appConf.config:
+        raise Exception("'hostip' not found in config")
+
+    hostip = appConf.config['mqhost']
+
+    # debug
+    if appConf.config['debug']:
+        blockTimer = CodeBlockTimer()
+        blockTimer.start()
 
     image_file = Path(image)
     file_size = image_file.stat().st_size
@@ -27,7 +40,7 @@ def send_file_over_mbroker(image: str, sensor: str):
         raise Exception("Cannot find specified image file")
 
     # Load the image to be sent
-    print(f"load the image to be sent from: ", image)
+    print(f"\tload the image to be sent from: ", image)
     with open(str(image_file), "rb") as f:
         bin_content = f.read()
 
@@ -54,6 +67,9 @@ def send_file_over_mbroker(image: str, sensor: str):
     # Publish the reponse
     channel.send_response(response)
 
+    #debug
+    if appConf.config['debug']: print(f"Total transfer time: ", blockTimer.stop(), "ms")
+
     print(
         f"""
 Successfully sent message
@@ -70,19 +86,23 @@ def archive_img(filepath):
 
     directory, filename = os.path.split(filepath)
     new_filename = str(timestamp) + "_" + str(filename)
-    print("new filename: ", new_filename)
+
+    if appConf.config['debug']: print("\tnew filename: ", new_filename)
 
     # Construct the new filepath
     new_filepath = os.path.join(directory, new_filename)
-    new_filepath = new_filepath.replace(".jpg", ".processJpg")
+    new_filepath = new_filepath.replace(appConf.config['mon_extension'], str(appConf.config['mon_extension']) + "Process")
     os.rename(filepath, new_filepath)
-    print("Processed and renamed: ", filepath)
+
+    if appConf.config['debug']: print("\tProcessed and renamed: ", filepath)
 
 
 # Simulate sending the file over WebSocket
 def mock_send_file_over_websocket(filepath):
-    print("Sending file over WebSocket: ", filepath)
+    print("\tSending file over WebSocket: ", filepath)
     time.sleep(2)  # Simulate delay
+
+    archive_img(filepath)
 
 # Monitor directory for new .jpg files
 def monitor_directory(directory):
@@ -99,18 +119,28 @@ def monitor_directory(directory):
             for flag in flags.from_mask(event.mask):
                 if flag == flags.CREATE:
                     filename = event.name
-                    if filename.endswith('.jpg'):
+                    if filename.endswith(appConf.config['mon_extension']):
                         filepath = os.path.join(directory, filename)
-                        print("found: ", filename)
+                        print("Found: ", filename)
                         monitored_file = filename
                 if flag == flags.CLOSE_WRITE:
                     if monitored_file == event.name:
                         # Start a new thread to handle the file processing
-                        #threading.Thread(target=mock_send_file_over_websocket, args=(filepath,)).start()
                         threading.Thread(target=send_file_over_mbroker, args=(filepath,"sensor1",)).start()
 
+# load config and set the default config parameter
+print(f"Loading configuration file..")
+appConf = Config()
+appConf.load("conf_fileMon.json")
+default_mon_extension = ".jpg"
+
 if __name__ == "__main__":
-    directory_to_monitor = "/opt/imgin/"
+    if 'mon_dir' not in appConf.config:
+        raise Exception("'mon_dir' not found in config")
+    directory_to_monitor = appConf.config['mon_dir']
+
+    if 'mon_extension' not in appConf.config:
+        appConf.config['mon_extension'] = default_mon_extension
 
     if not os.path.exists(directory_to_monitor) or not os.path.isdir(directory_to_monitor):
         print("The directory \'", directory_to_monitor, "\' does not exist or is not a directory!")
