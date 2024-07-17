4f42a61ebc69374d309a450d5cf519252b08b690
PheerawatB <pheerawat.b@riseplus.tech>
2024-07-17 17:26:12 +0700

    .

--- src/webapp/src/services/geoserver.service.ts
+++ src/webapp/src/services/geoserver.service.ts
@@ -13,9 +13,8 @@ import { Layer, LayerResponse } from '../models/layer.model';
 export class GeoServerService {
 
 
-  // private proxy = `http://${window.location.hostname}:8000/geoserver`;
-  // private  proxy = `http://139.59.221.224:8080/geoserver`;
-  private proxy = `http://167.172.94.39:8000/geoserver`;
+  private proxy = `http://${window.location.hostname}:8000/geoserver`;
+  // private proxy = `http://167.172.94.39:8000/geoserver`; 
 
   constructor(private http: HttpClient,
     private dialog: MatDialog, private shareService: SharedService) { }
