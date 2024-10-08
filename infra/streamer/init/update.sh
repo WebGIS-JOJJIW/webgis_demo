5ed2ce0fd46205d1f4937517b52b143f174d80e9
Kittisak Sajjapongse <kittisak.sajjapongse@gmail.com>
2024-10-08 16:55:10 -0400

    Added update script to migrate streamer database

---
+++ infra/streamer/init/update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
+SETUP_IMAGE=streamer_api:0.1
+
+set -e
+set -x
+
+sudo docker build -t $SETUP_IMAGE $SCRIPT_DIR/../streamer-api
+sudo docker run -it --rm --network webgis_net $SETUP_IMAGE rake db:migrate
+sudo docker run -it --rm --network webgis_net $SETUP_IMAGE rake db:seed
